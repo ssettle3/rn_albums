a1f1a906db8257ded6ecf55e602884268f65ead1
Stephen Settle <ssettle3@gmail.com>
2017-06-26 23:05:33 -0400

    AlbumList component and go fetch data with lifecycle function

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header title={'Albums'} />
+  <View>
+    <Header title={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+  state = { albums: [] }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then((response) => {
+      this.setState({ albums: response.data });
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List Here</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
